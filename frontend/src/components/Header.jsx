f8ee41f36b8f7b857bea83453a2d4860a4688dd8
Mateo Jimenez <mateojimenez37@gmail.com>
2023-05-30 14:07:30 -0300

    -- Barra de busqueda --

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,4 +1,4 @@
-import { Navbar, Nav, Container } from 'react-bootstrap';
+import { Navbar, Nav, Container, Button ,Form } from 'react-bootstrap';
 import { FaShoppingCart, FaUser } from 'react-icons/fa';
 import logo from "../assets/logo.png";
 import '../assets/styles/header_style.css';
@@ -17,6 +17,17 @@ const Header = () => {
                     <Nav className="ms-auto">
                         <Nav.Link href="/cart" className="navbar-item"><FaShoppingCart/> Carrito</Nav.Link>
                         <Nav.Link href="/login" className="navbar-item"><FaUser/> Ingresa</Nav.Link>
+                        <Form className="d-flex">
+                            <Form.Control
+                                type="search"
+                                placeholder="Buscar productos"
+                                className="me-2 rounded"
+                                aria-label="Search"
+                            />
+                            <Button className="rounded-pill" variant="outline-secondary">
+                             Buscar
+                            </Button>
+                        </Form>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
