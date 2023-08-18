b2df4ba1483e22ac7afe8f196a8a3440fd8bfe4f
Mateo Jimenez <mateojimenez37@gmail.com>
2023-08-17 23:45:23 -0300

    -- order controller --

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -58,7 +58,24 @@ const getOrderById = asyncHandler(async (req, res) => {
 
 //actualizar orden a pagado
 const updateOrderToPaid = asyncHandler (async (req, res) => {
-    res.send('update order to paid');
+    const order = await Order.findByIdId(req.params.id);
+
+    if (order) {
+      order.isPaid = true;
+      order.paidAt = Date.now();
+      order.paymentResult = {
+        id: req.body.id,
+        status: req.body.status,
+        update_time: req.body.update_time,
+        email_address: req.body.email_address,
+      };
+
+      const updatedOrder = await order.save();
+
+      res.status(200).json(updatedOrder);
+    } else {
+      throw new Error('Orden no encontrada');
+    }
   });
 
 //actualizar a enviado  
