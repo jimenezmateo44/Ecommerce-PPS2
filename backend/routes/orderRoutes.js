745c956e0e53978e11d92d33bc8f5e48c45460fe
Mateo Jimenez <88990332+jimenezmateo44@users.noreply.github.com>
2023-10-23 14:54:16 -0300

    Update orderRoutes.js

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,23 +1,23 @@
-// import express from "express";
-// import {
-//     addOrderItems,
-//     getMyOrders,
-//     getOrderById,
-//     updateOrderToPaid,
-//     updateOrderToDelivered,
-//     getOrders,
-// } from '../controllers/orderController.js';
-// import { protect, admin } from '../middleware/authMiddelware.js';
+import express from "express";
+import {
+    addOrderItems,
+    getMyOrders,
+    getOrderById,
+    updateOrderToPaid,
+    updateOrderToDelivered,
+    getOrders,
+} from '../controllers/orderController.js';
+import { protect, admin } from '../middleware/authMiddelware.js';
 
-// const router = express.Router();
+const router = express.Router();
 
-// router.route('/').post(protect, addOrderItems).get(protect, admin, getOrders);
-// router.route('/mine').get(protect, getMyOrders);
-// router.route('/:id').get(protect, getOrderById);
-// router.route('/:id/pay').put(protect, updateOrderToPaid);
-// router.route('/:id/deliver').put(protect, admin, updateOrderToDelivered);
-
-
+router.route('/').post(protect, addOrderItems).get(protect, admin, getOrders);
+router.route('/mine').get(protect, getMyOrders);
+router.route('/:id').get(protect, getOrderById);
+router.route('/:id/pay').put(protect, updateOrderToPaid);
+router.route('/:id/deliver').put(protect, admin, updateOrderToDelivered);
 
 
-// export default router;
+
+
+export default router;
