f4d18de339cb4d86bb6cabee8285e576649ee784
Mateo Jimenez <88990332+jimenezmateo44@users.noreply.github.com>
2023-10-23 14:53:11 -0300

    Update orderController.js

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,101 +1,101 @@
-// import asyncHandler from '../middleware/asyncHandler.js';
-// // import Order from '../models/orderModel.js';
+import asyncHandler from '../middleware/asyncHandler.js';
+import Order from '../models/orderModel.js';
 
-// //crear una orden
-// const addOrderItems = asyncHandler(async (req, res) => {
-//   const {
-//     orderItems,
-//     shippingAddress,
-//     paymentMethod,
-//     itemsPrice,
-//     shippingPrice,
-//     totalPrice,
-//   } = req.body;
+//crear una orden
+const addOrderItems = asyncHandler(async (req, res) => {
+  const {
+    orderItems,
+    shippingAddress,
+    paymentMethod,
+    itemsPrice,
+    shippingPrice,
+    totalPrice,
+  } = req.body;
 
-//   if (orderItems && orderItems.length === 0) {
-//     res.status(400);
-//     throw new Error('No order items');
-//   } else {
-//     const order = new Order({
-//       orderItems: orderItems.map((x) => ({
-//         ...x,
-//         product: x._id,
-//         _id: undefined,
-//       })),
-//       user: req.user._id,
-//       shippingAddress,
-//       paymentMethod,
-//       itemsPrice,
-//       shippingPrice,
-//       totalPrice,
-//     });
+  if (orderItems && orderItems.length === 0) {
+    res.status(400);
+    throw new Error('No order items');
+  } else {
+    const order = new Order({
+      orderItems: orderItems.map((x) => ({
+        ...x,
+        product: x._id,
+        _id: undefined,
+      })),
+      user: req.user._id,
+      shippingAddress,
+      paymentMethod,
+      itemsPrice,
+      shippingPrice,
+      totalPrice,
+    });
 
-//     const createdOrder = await order.save();
+    const createdOrder = await order.save();
 
-//     res.status(201).json(createdOrder);
-//   }
-// });
+    res.status(201).json(createdOrder);
+  }
+});
 
-// const getMyOrders = asyncHandler(async (req, res) => {
-//   const orders = await Order.find({ user: req.user._id });
-//   res.json(orders);
-// });
-
-
-// const getOrderById = asyncHandler(async (req, res) => {
-//   const order = await Order.findById(req.params.id).populate(
-//     'user',
-//     'name email'
-//   );
-
-//   if (order) {
-//     res.json(order);
-//   } else {
-//     res.status(404);
-//     throw new Error('Orden no encontrada');
-//   }
-// });
-
-// //actualizar orden a pagado
-// const updateOrderToPaid = asyncHandler (async (req, res) => {
-//     const order = await Order.findByIdId(req.params.id);
-
-//     if (order) {
-//       order.isPaid = true;
-//       order.paidAt = Date.now();
-//       order.paymentResult = {
-//         id: req.body.id,
-//         status: req.body.status,
-//         update_time: req.body.update_time,
-//         email_address: req.body.email_address,
-//       };
-
-//       const updatedOrder = await order.save();
-
-//       res.status(200).json(updatedOrder);
-//     } else {
-//       throw new Error('Orden no encontrada');
-//     }
-//   });
-
-// //actualizar a enviado  
-// const updateOrderToDelivered = asyncHandler (async (req, res) => {
-//     res.send('update order to delivered');
-//   });
+const getMyOrders = asyncHandler(async (req, res) => {
+  const orders = await Order.find({ user: req.user._id });
+  res.json(orders);
+});
 
 
-// //obtener todas las ordenes 
-// const getOrders = asyncHandler (async (req, res) => {
-//     const orders = await Order.find({}).populate('user', 'id name');
-//     res.status(200).json(orders);
-//   });
+const getOrderById = asyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id).populate(
+    'user',
+    'name email'
+  );
+
+  if (order) {
+    res.json(order);
+  } else {
+    res.status(404);
+    throw new Error('Orden no encontrada');
+  }
+});
+
+//actualizar orden a pagado
+const updateOrderToPaid = asyncHandler (async (req, res) => {
+    const order = await Order.findByIdId(req.params.id);
+
+    if (order) {
+      order.isPaid = true;
+      order.paidAt = Date.now();
+      order.paymentResult = {
+        id: req.body.id,
+        status: req.body.status,
+        update_time: req.body.update_time,
+        email_address: req.body.email_address,
+      };
+
+      const updatedOrder = await order.save();
+
+      res.status(200).json(updatedOrder);
+    } else {
+      throw new Error('Orden no encontrada');
+    }
+  });
+
+//actualizar a enviado  
+const updateOrderToDelivered = asyncHandler (async (req, res) => {
+    res.send('update order to delivered');
+  });
 
 
-// export {
-//     addOrderItems,
-//     getMyOrders,
-//     getOrderById,
-//     updateOrderToPaid,
-//     updateOrderToDelivered,
-//     getOrders,
-// };
+//obtener todas las ordenes 
+const getOrders = asyncHandler (async (req, res) => {
+    const orders = await Order.find({}).populate('user', 'id name');
+    res.status(200).json(orders);
+  });
+
+
+export {
+    addOrderItems,
+    getMyOrders,
+    getOrderById,
+    updateOrderToPaid,
+    updateOrderToDelivered,
+    getOrders,
+};
